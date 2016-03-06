0be12175607e165efee646cd6ba72ecb99cdd77a
hao <xinhao@brandeis.edu>
2016-03-06 18:39:57 -0500

    Added script for player tag preprocessing

---
+++ preprocess/tagPlayer.py
@@ -0,0 +1,40 @@
+from operator import itemgetter
+import re
+
+def inside(num, spans):
+    for span in spans:
+        if num>=span[0] and num<span[1]:
+            return True
+    return False
+
+def tagPlayer(doc, players):
+    
+    with open(doc,"r",-1,"UTF-8") as f:
+        content=f.read()
+
+    matchlist = []
+    for fn in players:
+        m1=re.finditer(fn, content)
+        for match1 in m1:
+            matchlist.append([match1.span(),match1.group()])
+        m2=re.finditer(fn.split()[-1], content)
+        for match2 in m2:
+            spans = [itemgetter(0)(item) for item in matchlist]
+            if not inside(match2.span()[0],spans):
+                matchlist.append([match2.span(),match2.group()])
+
+    matchlist=sorted(matchlist, key=itemgetter(0))
+
+    with open(doc+".xml","w",-1,"UTF-8") as f:
+        f.write("""<?xml version="1.0" encoding="UTF-8" ?>\n\n<SoccEval1.1>\n<TEXT><![CDATA[""")
+        f.write(content+"]]></TEXT>\n")
+        f.write("<TAGS>\n")
+        for i in range(len(matchlist)):
+            f.write("""<Player id="%s" spans="%d~%d" text="%s" playerID="null" />\n"""%("P"+str(i),matchlist[i][0][0],matchlist[i][0][1],matchlist[i][1]))
+        f.write("</TAGS>\n</SoccEval1.1>")
+
+if __name__ == "__main__":
+    with open("./playerlist.txt","r",-1,"UTF-8") as f:
+        players = f.read().split("\n")
+        players.remove("")
+    tagPlayer("./test.txt", players)
