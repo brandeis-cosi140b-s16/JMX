6945595ce735ab55e26137e688761e053e08dfd6
Matthew_Garber <mgarber@brandeis.edu>
2016-05-09 22:51:28 -0400

    Revamped baseline scripts to use cross-validation and sklearn classifiers.

---
+++ baseline/create_baseline_corpus2.py
@@ -0,0 +1,243 @@
+#!python3
+"""
+Author: Matthew Garber
+Class: Natural Language Annotation for Machine Learning
+Term: Spring 2016
+Project: SoccEval
+
+This program creates a JSON corpus of sentences regarding a particular player
+during a given match and that player's rating for that match, which can be used
+to train a classifier. The corpus is saved as ratings_corpus.json.
+
+To run:
+    python create_baseline_corpus.py
+
+"""
+
+import json
+import os
+import re
+import unicodedata
+from nltk import sent_tokenize
+from urllib.parse import unquote_plus
+
+coreferential_terms = ['defender', 'forward' 'goalie', 'goalkeeper',
+                       'he', 'him', 'his', 'keeper', 'midfielder',
+                       'player', 'striker']
+pattern_string = r'\b(' + '|'.join(coreferential_terms) + r')\b' 
+coref_pattern = re.compile(pattern_string, flags=re.I)
+
+def get_match_filepath(ratings_path, source):
+    """Retrieves the filepath for the match report corresponding to a given
+    ratings file.
+
+    Args:
+        rating_path: the full filepath to the ratings file.
+        source: the source of the match report, either 'goal' or 'guardian'
+    Returns:
+        a string filepath to the match report
+    """
+    text_match = re.search(r'/[^/]*$', ratings_path)
+    ratings_filename = text_match.group(0)
+    match_filename = ratings_filename[2:]
+    match_path = ''
+    if source == 'goal':
+        match_path = r'../goal_articles/all/' + match_filename
+    elif source == 'guardian':
+        date_match = re.match(r'^(\d{4})(\d{2})(\d{2})', match_filename)
+        date = '-'.join(date_match.groups())
+        filename_with_spaces = match_filename.replace('-', ' ')
+        team1, team2 = re.search(r'([a-z\- ]+)? vs ([a-z\- ]+)',
+                                 filename_with_spaces).groups()
+        team_dict = {
+            'queens park rangers': 'qpr',
+            'tottenham hotspur': 'tottenham',
+            'west ham united': 'west ham',
+            }
+        if team1 in team_dict:
+            team1 = team_dict[team1]
+        if team2 in team_dict:
+            team2 = team_dict[team2]
+        guardian_root = r'../guardian_articles/matchreportsCLT/'
+        for root, dirs, files in os.walk(guardian_root):
+            if root == guardian_root:
+                for f in files:
+                    if f.startswith(date):
+                        match1 = re.search(team1, f, re.I)
+                        match2 = re.search(team2, f, re.I)
+                        if match1 and match2:
+                            match_path = guardian_root + f
+    else:
+        raise Exception("source must be 'goal' or 'guardian'")
+    return match_path
+
+def parse_ratings(filename, ratings_file):
+    """Parses the ratings file, returning a dictionary mapping a tuple of the
+    player's full, first, and last names to the corresponding rating.
+
+    Args:
+        ratings_file: a file containing the rating of soccer players for a
+            given match.
+    Returns:
+        A dicitionary mapping a player_id to a their rating and tuple of the
+        player's names 
+    """
+    player_info = {}
+    pattern = re.compile(r'^href="/en-us/people/[\w-]+/\d+/(.*?)(-(.*?))?" data-rate="(\d\.\d)"')
+    
+    base_player_id = 0
+    for line in ratings_file:
+        player_id = '{}_{}'.format(filename, base_player_id)
+        clean_line = strip_accents(unquote_plus(line))
+        match = re.match(pattern, clean_line)
+        first_name = match.group(1)
+        if match.group(3):
+            last_name = match.group(3)
+        else:
+            last_name = first_name
+        full_name = ' '.join([first_name, last_name])
+        names = tuple(wordify(word) for word in [full_name, first_name, last_name])
+        rating = float(match.group(4))
+        player_info[player_id] = {
+            'names': names,
+            'rating': rating
+            }
+        base_player_id += 1
+
+    return player_info
+
+def create_subdocs(match_text, player_info, source):
+    """Creates a sub-document for each player mentioned in the document, and
+    returns a list of tuples of sub-documents and player ratings.
+
+    Sentences that mention multiple players will be added multiple times,
+    regardless of whether they are rated the same or differently.
+
+    Args:
+        match_text: text of a match report article.
+        player_info: a dict mapping plauer_id to a rating and names fields.
+        source: 'goal' if the match text is from a goal.com article or
+            'guardian' if it is from a Guardian article.
+    Returns:
+        A dict pairing sentences about a player with that player's rating for
+        the match.
+    """
+    if source not in ('goal', 'guardian'):
+        raise Exception('Invalid source')
+    cleaned_text = remove_header(match_text, source)
+    cleaned_text = strip_accents(cleaned_text)
+    sents = sent_tokenize(cleaned_text)
+
+    subdocs_by_player = {}
+    latest = [-1, -1]   # [player_id, span_end]
+    has_player_name = False
+    for sent in sents:
+        for player_id, fields in player_info.items():
+            i = 0
+            match = False
+            while i < 3 and not match:
+                match = re.search(player_info[player_id]['names'][i], sent, re.I)
+                i += 1
+            if match:
+                has_player_name = True
+                dict_append(subdocs_by_player, player_id, sent)
+                if latest[1] > match.end():
+                    latest = [player_id, match.end()]
+        if not has_player_name:
+            match = re.search(coref_pattern, sent)
+            if match and latest[1] > -1:
+                dict_append(subdocs_by_player, player_id, sent)
+                
+    # Convert list of sents into a single string
+    for player_id, sents in subdocs_by_player.items():
+        text = ' '.join(subdocs_by_player[player_id])
+        rating = player_info[player_id]['rating']
+        subdocs_by_player[player_id] = [text, rating]
+    return subdocs_by_player
+
+def create_ratings_corpus(ratings_filenames, source):
+    """Creates a labeled corpus of sub-documents from documents corresponding
+    to the given list of ratings file filenames.
+    
+    For each player in each document, all sentences regarding that player are
+    concatenated into a sub-document and labeled with that player's rating for
+    that match.
+
+    Args:
+        ratings_filenames: a list of filenames of ratings files.
+        source: 'goal' to use source documents from goal.com or 'guardian' to
+            use source documents from the Guardian.
+    Returns:
+        The corpus mapping a player_id to a tuple of text about that player
+        during a particular match and that player's rating for that match.
+    """
+    subdocs = {}
+    for filepath in ratings_filenames:
+        ratings_file = open(filepath)
+        player_info = parse_ratings(filepath, ratings_file)
+        match_filepath = get_match_filepath(filepath, source)
+        if match_filepath and os.path.exists(match_filepath):
+            match_text = open(match_filepath, encoding='utf8').read()
+            subdocs.update(create_subdocs(match_text, player_info, source))
+    return subdocs
+
+def strip_accents(string):
+    """Removes accents from a given string.
+    """
+    return ''.join(c for c in unicodedata.normalize('NFD', string)
+                  if unicodedata.category(c) != 'Mn')
+
+def wordify(word):
+    """Adds regex word boundary symbols to the start and end of the given
+    string.
+    """
+    return r'\b' + word + r'\b'
+
+def dict_append(dict, key, value):
+    """Appends a value to the list corresponding to the given key in the given
+    dictionary. Creates a new list if there is no such key.
+    """
+    if key not in dict:
+        dict[key] = [value]
+    else:
+        dict[key].append(value)
+
+def remove_header(text, source):
+    """Removes the header from the text of a given match report. 'source'
+    designates the source of that match report.
+    """
+    pattern = ''
+    if source == 'goal':
+        pattern = re.compile(r'^.*?[AP]M\n', re.DOTALL)
+    elif source == 'guardian':
+        pattern = re.compile(r'^.*?\n')
+    else:
+        raise Exception("source must be 'goal' or 'guardian'")
+    cleaned_text = re.sub(pattern, '', text)
+    return cleaned_text        
+
+
+def main():
+    ratings_dir = r'../ratings/'
+    filepaths = []
+    for root, dirs, files in os.walk(ratings_dir):
+        for filename in files:
+            if filename.endswith('txt'):
+                filepaths.append(os.path.join(root, filename))
+    
+    corpus = create_ratings_corpus(filepaths, 'goal')
+    guardian_corpus = create_ratings_corpus(filepaths, 'guardian')
+    for player_id in guardian_corpus:
+        if player_id in corpus:
+            goal_text = corpus[player_id][0]
+            guardian_text = guardian_corpus[player_id][0]
+            corpus[player_id][0] = ' '.join([goal_text, guardian_text])
+        else:
+            corpus[player_id] = guardian_corpus[player_id]
+    
+    corpus_file = open('ratings_corpus2.json', mode='w')
+    json.dump(corpus, corpus_file, indent=4)
+    corpus_file.close()
+    
+if __name__ == '__main__':
+    main()
---
+++ baseline/test_baseline_cross_validate.py
@@ -0,0 +1,347 @@
+#!python3
+"""
+Author: Matthew Garber
+Class: Natural Language Annotation for Machine Learning
+Term: Spring 2016
+Project: SoccEval
+
+This program either trains a classifier or loads an existing one and tests it
+via k-fold cross validation on a portion of the documents from the file
+ratings_corpus2.json, printing information regarding the classifier's averaged
+precision and recall for each class.
+
+NOTE: Loading previously trained classifiers doesn't not currently work correctly.
+
+To run:
+    python test_baseline.py <command> <classifier>
+
+<command> should be:
+    - 'new' to create the classifier from scratch.
+    - 'load' to load an existing classifier.
+<classifier> should be
+    - 'maxent' to create or load a MaxEnt classifier.
+    - 'decision_tree' to create or load a decision tree classifier.
+    - 'lr' to create or load a linear regression classifier.
+    - 'svm' to create or load a support vector machine classifier.
+"""
+
+import json, nltk, pickle, re, sys
+from itertools import combinations
+from nltk import FreqDist
+from nltk.corpus import stopwords
+from nltk.metrics import scores
+from nltk.tokenize import word_tokenize
+from sklearn import ensemble, linear_model, svm, tree
+from sklearn.metrics import precision_recall_fscore_support
+
+STOPWORDS = stopwords.words('english')
+STOPWORDS.extend([',', '.'])
+
+ALL_RATINGS = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+
+def unigram_boolean(document):
+    """Coverts a document into a dict of unigram Boolean features.
+
+    Args:
+        document: some document's text (as a string)
+    Returns:
+        A dict mapping each non-stopword token to True.
+    """
+    tokens = [token.lower() for token in word_tokenize(document)
+              if token.lower() not in STOPWORDS]
+    return {token: True for token in tokens}
+
+def unigram_freqs(document):
+    """Coverts a document into a unigram frequency distribution that can be
+    used as features for a classifier.
+
+    Args:
+        document: some document's text (as a string)
+    Returns:
+        A FreqDist of non-stopword tokens in the document.
+    """
+    tokens = [token.lower() for token in word_tokenize(document)
+              if token.lower() not in STOPWORDS]
+    return FreqDist(tokens)
+
+def select_folds(input_list, k=10):
+    """Separates a list into folds for cross-validation.
+
+    Args:
+        input_list: a lsit to separate into k folds.
+        k: the number of folds to create.
+    Returns:
+        A list of k lists, each comprising a fold.
+    """
+    fold_size = len(input_list) // k
+    folds = []
+    for i in range(0, len(input_list), fold_size):
+        fold = input_list[i:i+k]
+        if len(fold) < fold_size:
+            for x in range(len(fold)):
+                folds[x].append(fold[x])
+        else:
+            folds.append(fold)
+    return folds
+
+def extract_features(document, feature_functions):
+    """Extracts features from the document using the given feature functions.
+
+    Args:
+        document: some document's text (as a string)
+        feature_functions: a list of feature functions
+    Returns:
+        A dict of features for the given document.
+    """
+    features = {}
+    for feature_function in feature_functions:
+        features.update(feature_function(document))  
+    return features
+
+def split_data(feature_representation, test_portion=0.1):
+    """Separates the given document featuresets into a test set and a training
+    set. At least one document for each label will be included in the test set.
+
+    Args:
+        feature_representation: a list of document feature sets.
+        test_portion: the portion of documents to place in the test set.
+    Returns:
+        A tuple of the training set and test set.
+    """
+    if test_portion >= 1:
+        raise Exception('test_portion must be less than 1!')
+    cutoff = int(test_portion * len(feature_representation))
+    #train_set = feature_representation[cutoff:]
+    #test_set = feature_representation[:cutoff]
+    train_set = []
+    test_set = []
+    for label in ALL_RATINGS:
+        rating_set = [pair for pair in feature_representation
+                      if pair[1] == label]
+        cutoff = int(test_portion * len(rating_set))
+        if cutoff == 0:
+            cutoff += 1
+        train_set.extend(rating_set[cutoff:])
+        test_set.extend(rating_set[:cutoff])
+    return train_set, test_set
+
+def initialize_sets(labels):
+    """Creates a dict mapping each item in labels to an empty set.
+    """
+    return {label : set() for label in labels}
+
+def update_scores(avg_scores, fold_scores):
+    """Updates the overall scores by adding the scores the the last iteration
+    of the k-fold cross-validation.
+
+    Args:
+        avg_scores: the average cross validation scores
+        fold_scores: the scores from the a cross validation iteration
+    """
+    avg_scores['micro']['p'] += fold_scores['micro'][0]
+    avg_scores['micro']['r'] += fold_scores['micro'][1]
+    avg_scores['micro']['f'] += fold_scores['micro'][2]
+    avg_scores['macro']['p'] += fold_scores['macro'][0]
+    avg_scores['macro']['r'] += fold_scores['macro'][1]
+    avg_scores['macro']['f'] += fold_scores['macro'][2]
+    for rating in ALL_RATINGS:
+        avg_scores['by_label'][rating]['p'] += fold_scores['by_label'][rating]['p']
+        avg_scores['by_label'][rating]['r'] += fold_scores['by_label'][rating]['r']
+        avg_scores['by_label'][rating]['f'] += fold_scores['by_label'][rating]['f']
+        
+def average_scores(avg_scores, k=10):
+    """Averages the overall scores from the k-fold cross validation.
+
+    Args:
+        avg_scores: the raw scores.
+    """
+    avg_scores['micro']['p'] /= k
+    avg_scores['micro']['r'] /= k
+    avg_scores['micro']['f'] /= k
+    avg_scores['macro']['p'] /= k
+    avg_scores['macro']['r'] /= k
+    avg_scores['macro']['f'] /= k
+    for rating in ALL_RATINGS:
+        avg_scores['by_label'][rating]['p'] /= k
+        avg_scores['by_label'][rating]['r'] /= k
+        avg_scores['by_label'][rating]['f'] /= k
+
+def test_iteration(i, train_set, test_dict, feature_sets_by_match,
+                   classifier_type='decision_tree', command='new'):
+    """Performs one iteration of the k-fold cross validation, returing a dict
+    containing overall micro and macro score averages, in addition to scores for
+    each label.
+
+    For efficient retesting, this function saves pickled versions of classifier
+    objects if they are trained from scratch, as they take some time to train.
+
+    Args:
+        i: the iteration of the k-fold cross validation.
+        train_set: a list containing feature, rating pairs
+        test_dict: a dicitonary containing feature and rating information for
+            the test set.
+        feature_sets_by_match: feature respresentations of documents organized
+            by match.
+        classifier: the type of classifier to use.
+        command: 'new' to train and save a new classifier, 'load' to use a
+            previously trained classifier.
+    Returns:
+        A dict containing overall micro and macro score averages, in addition
+        to scores for each label.
+    """
+    #classifier = nltk.classify.scikitlearn.SklearnClassifier(svm.SVR()).train(train_set)
+    classifier = ''
+    if command == 'new':
+        if classifier_type == 'decision_tree':
+            #classifier = nltk.classify.DecisionTreeClassifier.train(train_set)
+            classifier = nltk.classify.scikitlearn.SklearnClassifier(tree.DecisionTreeClassifier()).train(train_set)
+        elif classifier_type == 'maxent':
+            #classifier = nltk.classify.maxent.MaxentClassifier.train(train_set)
+            classifier = nltk.classify.scikitlearn.SklearnClassifier(linear_model.LogisticRegression()).train(train_set)
+        elif classifier_type == 'svm':
+            classifier = nltk.classify.scikitlearn.SklearnClassifier(svm.SVR()).train(train_set)
+        classifier_file = open('_'.join([classifier_type, str(i)]), 'wb')
+        pickle.dump(classifier, classifier_file)
+        classifier_file.close()
+    elif command == 'load':
+        classifier_file = open('_'.join([classifier_type, str(i)]), 'rb')
+        classifier = pickle.load(classifier_file)
+        classifier_file.close()
+    
+    pred_sets = initialize_sets(ALL_RATINGS)
+    gold_sets = initialize_sets(ALL_RATINGS)
+    pred_list = []
+    gold_list = []
+
+    # Classify predictions and add them to relvant dicts and lists.
+    for match in test_dict:
+        for doc_id in test_dict[match]:
+            test_doc = test_dict[match][doc_id]['features']
+            pred = classifier.classify(test_doc)
+            gold = test_dict[match][doc_id]['gold']
+            test_dict[match][doc_id]['pred'] = pred
+
+            gold_list.append(str(gold))
+            pred_list.append(str(pred))
+            gold_sets[gold].add(doc_id)
+            pred_sets[pred].add(doc_id)
+
+    # Calculate pairwise ranking accuracy
+    correct= 0
+    total = 0
+    for match in test_dict:
+        for pl1, pl2 in combinations(test_dict[match].keys(), 2):
+            p1 = test_dict[match][pl1]
+            p2 = test_dict[match][pl2]
+            if p1['gold'] > p2['gold'] and p1['pred'] > p2['pred']:
+                correct += 1
+            elif p1['gold'] < p2['gold'] and p1['pred'] < p2['pred']:
+                correct += 1
+            elif p1['gold'] == p2['gold'] and p1['pred'] == p2['pred']:
+                correct += 1
+            total += 1
+
+    print('Pairwise ranking accuracy: ' + str(correct/total))
+    
+    fold_scores = {'micro': '',
+                   'macro': '',
+                   'by_label': {rating: {'p': 0, 'r': 0, 'f': 0}
+                                for rating in ALL_RATINGS}
+                   }
+    prf_micro = precision_recall_fscore_support(gold_list, pred_list, average='micro')
+    print(prf_micro)
+    fold_scores['micro'] = prf_micro
+
+    prf_macro = precision_recall_fscore_support(gold_list, pred_list, average='macro')
+    print(prf_macro)
+    fold_scores['macro'] = prf_macro
+
+    for label in ALL_RATINGS:
+        r = scores.recall(gold_sets[label], pred_sets[label])
+        p = scores.precision(gold_sets[label], pred_sets[label])
+        f = scores.f_measure(gold_sets[label], pred_sets[label])
+        
+        if r == None:
+            r = 0.0
+        if p == None:
+            p = 0.0
+        if f == None:
+            f = 0.0
+            
+        fold_scores['by_label'][label]['p'] = p
+        fold_scores['by_label'][label]['r'] = r
+        fold_scores['by_label'][label]['f'] = f
+        f = float(f)
+        print('<{}> P: {:.2}, R: {:.2}, F: {:.2}'.format(label, p, r, f))
+
+    return fold_scores
+
+def main(command, classifier_type):
+    
+    feature_functions = [unigram_boolean]
+    
+    corpus_file = open('ratings_corpus2.json')
+    corpus = json.load(corpus_file)
+    corpus_file.close()
+
+    matches = set()
+    for doc_id in corpus:
+        matches.add(doc_id.split('_')[0])
+    folds = select_folds(list(matches))
+    
+    feature_sets_by_match = {}
+    for doc_id, entry in corpus.items():
+        text = entry[0]
+        rating = entry[1]
+        features = extract_features(text, feature_functions)
+
+        prefix = doc_id.split('_')[0]
+        if prefix not in feature_sets_by_match:
+            feature_sets_by_match[prefix] = {doc_id: (features, rating)}
+        else:
+            feature_sets_by_match[prefix][doc_id] = (features, rating)
+
+    avg_scores = {'micro': {'p': 0, 'r': 0, 'f': 0},
+                  'macro': {'p': 0, 'r': 0, 'f': 0},
+                  'by_label': {rating: {'p': 0, 'r': 0, 'f': 0}
+                               for rating in ALL_RATINGS}
+                  }
+    
+    for i in range(len(folds)):
+        train_folds = folds[:i]
+        train_folds.extend(folds[i+1:])
+        train_set = []
+        for fold in train_folds:
+            for match in fold:
+                for doc_id in feature_sets_by_match[match]:
+                    train_set.append(feature_sets_by_match[match][doc_id])
+        test_dict = {}
+        for match in folds[i]:
+            test_dict[match] = {}
+            for doc_id in feature_sets_by_match[match]:
+                test_dict[match][doc_id] = {'features': feature_sets_by_match[match][doc_id][0]}
+                test_dict[match][doc_id]['gold'] = feature_sets_by_match[match][doc_id][1]
+
+        print('iteration {}:'.format(i))
+        fold_scores = test_iteration(i, train_set, test_dict, feature_sets_by_match,
+                                     classifier_type=classifier_type, command=command)
+        update_scores(avg_scores, fold_scores)
+        
+    average_scores(avg_scores)
+    micro = [avg_scores['micro'][label] for label in ['p', 'r', 'f']]
+    macro = [avg_scores['macro'][label] for label in ['p', 'r', 'f']]
+    print('MICRO P: {:.2}, R: {:.2}, F: {:.2}'.format(micro[0], micro[1], micro[2]))
+    print('MACRO P: {:.2}, R: {:.2}, F: {:.2}'.format(macro[0], macro[1], macro[2]))
+    for label in ALL_RATINGS:
+        p = avg_scores['by_label'][label]['p']
+        r = avg_scores['by_label'][label]['r']
+        f = avg_scores['by_label'][label]['f']
+        print('<{}> P: {:.2}, R: {:.2}, F: {:.2}'.format(label, p, r, f))
+    
+
+if __name__ == '__main__':
+    command = sys.argv[1]
+    classifier_type = sys.argv[2]
+    if command not in ['new', 'load'] or classifier_type not in ['decision_tree', 'lr', 'maxent', 'svm']:
+        raise Exception('Invalid command-line argument')
+    main(command, classifier_type)
